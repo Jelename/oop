e88c6d06060d613d5443148af2688bebdc5c087b
Jelename <yelena.medenica@gmail.com>
2018-06-04 14:36:07 +0200

    test3 done

---
+++ src/data/getUserBirthDate.js
@@ -0,0 +1,7 @@
+module.exports = function(user) {
+  if(user.birthday) {
+    return user.birthday;
+  } else {
+    return Date.parse(user.dateOfBirth);
+  }
+}
--- src/printAges.spec.js
+++ src/printAges.spec.js
@@ -24,6 +24,7 @@ function check(output) {
     }
   })
 }
+
 describe("printAges", function() {
   check(printAges(users1.concat(users2)))
 })
